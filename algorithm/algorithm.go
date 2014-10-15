4acd46f08ae2c4170b39157c94579c36d5a44553
flower-pot <flower-pot@flowerpot.io>
2014-10-15 14:56:24 +0200

    minimize loop checking for prime

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -1,14 +1,18 @@
 package algorithm
 
+import (
+	"math"
+)
 
 func IsPrime(n int) bool {
-	if n < 2 || n == 4 {
+	if n < 2 || n == 4 || n == 6 {
 		return false
 	}
-	if n == 2{
+	if n == 2 {
 		return true
 	}
-	for i := 3; i < n; i += 2 {
+	end := int(math.Ceil(math.Sqrt(float64(n))))
+	for i := 3; i < end; i += 2 {
 		if n%i == 0 {
 			return false
 		}
